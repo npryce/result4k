0003e7d80a449b7691b6e56ccba790fde55f551b
Nat Pryce <sw@natpryce.com>
2017-08-24 10:28:54 +0100

    Move functions to/from nullable values into their own file

--- src/main/kotlin/com/natpryce/result.kt
+++ src/main/kotlin/com/natpryce/result.kt
@@ -70,29 +70,3 @@ inline fun <T, E> Result<T, E>.peek(f: (T) -> Unit) =
 inline fun <T, E> Result<T, E>.peekError(f: (E) -> Unit) =
     apply { if (this is Err<E>) f(reason) }
 
-
-/*
- * Translate between the Result and Nullable/Optional/Maybe monads
- */
-
-/**
- * Convert a nullable value to a Result, using the result of the errorSupplier as the error reason if the value is null.
- */
-inline fun <T, E> T?.asResultOr(errorSupplier: () -> E) =
-    if (this != null) Ok(this) else Err(errorSupplier())
-
-/**
- * Returns the success value, or null if the Result is an error.
- */
-fun <T, E> Result<T, E>.valueOrNull() = when (this) {
-    is Ok<T> -> value
-    is Err<E> -> null
-}
-
-/**
- * Returns the error reason, or null if the Result is a success.
- */
-fun <T, E> Result<T, E>.errorOrNull() = when (this) {
-    is Ok<T> -> null
-    is Err<E> -> reason
-}
